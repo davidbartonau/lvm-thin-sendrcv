22239af2ec3d13df93ac94bd5b6539551664ba65
David Barton <dave@oneit.com.au>
2019-06-22 21:07:20 +0800

    Split out LVM Functionality into separate class

---
+++ src/oneit/lvmsendrcv/LVM.java
@@ -0,0 +1,183 @@
+package oneit.lvmsendrcv;
+
+import java.io.*;
+import java.util.*;
+import org.json.*;
+
+/**
+ *
+ * @author david
+ */
+public class LVM 
+{
+    /**
+     * getThinPoolInfo ("volg", "volg-thinpool")
+     * @param vgName
+     * @param thinPool
+     * @return
+     */
+    public static LVMThinPool getThinPoolInfo (String vgName, String thinPool) throws LVMException
+    {
+        // EXEC lvs --reportformat json -o chunksize,lv_dm_path ${thinPool}
+        List<JSONObject>            thinPools = execLVS(LVMThinPool.LVS_COLUMNS, new String[] { vgName + "/" + thinPool });
+
+        if (thinPools.size() != 1)
+        {
+            throw new LVMException("Thinpools.length != 1 : " + thinPools.size() + " " + thinPools);
+        }
+        
+        return new LVMThinPool(thinPools.get(0));
+    }
+    
+    
+    /**
+     * getSnapshotInfo ("volg", "thin_volume_snap2", "thin_volume_snap1")
+     * @param vgName
+     * @param snapshot1
+     * @param snapshot2
+     * @return map of LVMSnapshot keyed by name
+     */
+    public static Map<String, LVMSnapshot> getSnapshotInfo (String vgName, String snapshot1, String snapshot2) throws LVMException
+    {
+        // EXEC lvs --reportformat json -o lv_name,vg_name,pool_lv,thin_id volg/thin_volume_snap2 volg/thin_volume_snap1
+        List<JSONObject>            snapshots = execLVS(LVMSnapshot.LVS_COLUMNS, new String[] { vgName + "/" + snapshot1, vgName + "/" + snapshot2 });
+        Map<String, LVMSnapshot>    result = new HashMap<>();
+
+        for (JSONObject  lvsnapJSON : snapshots)
+        {
+            LVMSnapshot lvsnap = new LVMSnapshot(lvsnapJSON);
+            
+            result.put(lvsnap.snapshotName, lvsnap);
+        }
+        
+        return result;
+    }
+
+    
+    public static List<JSONObject> execLVS (String columns, String[] vols) throws LVMException
+    {
+        List<String>    lvsArgs         = new ArrayList<>();
+        String          lvsOutputStr;
+        
+        lvsArgs.addAll(Arrays.asList("/sbin/lvs", "--reportformat", "json", "-o", columns));
+        lvsArgs.addAll(Arrays.asList(vols));
+        
+        try
+        {
+            byte[]          lvsOutput           = new ExecUtils.ExecuteProcess ("/tmp/", lvsArgs.toArray(new String[0])).setHideStdOut(true).executeAsBytes();
+            
+            lvsOutputStr        = new String (lvsOutput);
+        }
+        catch (Exception e)
+        {
+            System.err.println("problem running lvs:" + e.getMessage()); //@todo
+            throw new LVMException(e);
+        }
+        
+        try
+        {
+            JSONObject          lvsOutputJSON   = new JSONObject(lvsOutputStr);
+            JSONArray           lvsOutputArray  = lvsOutputJSON.getJSONArray("report").getJSONObject(0).getJSONArray("lv");
+            List<JSONObject>    result          = new ArrayList<>();
+
+            for (int x = 0 ; x < lvsOutputArray.length() ; ++x)
+            {
+                JSONObject  outputRow = lvsOutputArray.getJSONObject(x);
+                
+                result.add (outputRow);
+            }
+
+            return result;
+        }
+        catch (Exception e)
+        {
+            System.err.println("problem parsing lvs output:" + e.getMessage() + "\n" + lvsOutputStr); //@todo
+            throw new LVMException(e);
+        }
+        
+    }
+
+    public static class LVMSnapshot
+    {
+        static final String LVS_COLUMNS = "lv_name,vg_name,pool_lv,thin_id";
+        
+        public final String  snapshotName;
+        public final String  vgName;
+        public final String  poolLV;
+        public final int     thinID;
+
+        public LVMSnapshot(JSONObject lvsnap) throws LVMException
+        {
+            try
+            {
+                this.snapshotName   = lvsnap.getString("lv_name");
+                this.vgName         = lvsnap.getString("vg_name");
+                this.poolLV         = lvsnap.getString("pool_lv");
+                this.thinID         = Integer.parseInt(lvsnap.getString("thin_id"));
+            }
+            catch (Exception e)
+            {
+                System.err.println("problem parsing lvs output:" + e.getMessage() + "\n" + lvsnap); //@todo
+                throw new LVMException(e);
+            }
+        }
+
+        
+        @Override
+        public String toString() 
+        {
+            return "LVSnapshot " + vgName + "/" + snapshotName + " in " + poolLV + "@" + thinID;
+        }
+    }
+    
+    
+    public static class LVMThinPool
+    {
+        static final String LVS_COLUMNS = "lv_name,vg_name,chunksize,lv_dm_path";
+        
+        public final String  name;
+        public final String  vgName;
+        public final int     chunkSizeKB;
+        public final String  dmPath;
+
+        public LVMThinPool(JSONObject thinJSON) throws LVMException
+        {
+            try
+            {
+                String      chunkSizeStr        = thinJSON.getString("chunk_size");
+
+                assert chunkSizeStr.charAt(chunkSizeStr.length() - 1) == 'k' : "Chunk size must be in kB";
+
+                this.name           = thinJSON.getString("lv_name");
+                this.vgName         = thinJSON.getString("vg_name");
+                this.chunkSizeKB    = (int)Float.parseFloat(chunkSizeStr.substring(0, chunkSizeStr.length() - 1));
+                this.dmPath         = thinJSON.getString("lv_dm_path");
+            }
+            catch (JSONException e)
+            {
+                System.err.println("problem parsing lvs output:" + e.getMessage() + "\n" + thinJSON); //@todo
+                throw new LVMException(e);
+            }
+        }
+        
+        @Override
+        public String toString() 
+        {
+            return "LVThinPool " + vgName + "/" + name + "  " + chunkSizeKB + " @ " + dmPath;
+        }
+    }
+
+    
+    public static class LVMException extends Exception
+    {
+        public LVMException(Exception cause) 
+        {
+            super (cause);
+        }
+        
+        public LVMException(String message) 
+        {
+            super (message);
+        }
+    }
+}
--- src/oneit/lvmsendrcv/LVMBlockDiff.java
+++ src/oneit/lvmsendrcv/LVMBlockDiff.java
@@ -15,14 +15,14 @@ import org.xml.sax.SAXException;
  */
 public class LVMBlockDiff 
 {
-    public static LVMSnapshotDiff diffLVMSnapshots (String vgName, String snapshot1, String snapshot2) throws InterruptedException, IOException, JSONException, ParserConfigurationException, SAXException, XPathExpressionException
+    public static LVMSnapshotDiff diffLVMSnapshots (String vgName, String snapshot1, String snapshot2) throws LVM.LVMException, InterruptedException, IOException
     {
-        Map<String, LVMSnapshot>    snapshotInfo = getSnapshotInfo(vgName, snapshot1, snapshot2);
+        Map<String, LVM.LVMSnapshot>    snapshotInfo = LVM.getSnapshotInfo(vgName, snapshot1, snapshot2);
         
         System.err.println (snapshotInfo); // @todo
         
         String          thinPoolName    = snapshotInfo.get(snapshot1).poolLV;
-        LVMThinPool     thinPool        = getThinPoolInfo(vgName, thinPoolName);
+        LVM.LVMThinPool thinPool        = LVM.getThinPoolInfo(vgName, thinPoolName);
         String          snap1ID         = String.valueOf(snapshotInfo.get(snapshot1).thinID);
         String          snap2ID         = String.valueOf(snapshotInfo.get(snapshot2).thinID);
 
@@ -44,37 +44,50 @@ public class LVMBlockDiff
     }
 
     
-    private static List<Integer> getBlocksDiffMatching (byte[] thin_delta_output, boolean includeSame, boolean includeLeftOnly, boolean includeRightOnly, boolean includeDifferent) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException
+    private static List<Integer> getBlocksDiffMatching (byte[] thin_delta_output, boolean includeSame, boolean includeLeftOnly, boolean includeRightOnly, boolean includeDifferent) throws LVM.LVMException
     {
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder        dBuilder = dbFactory.newDocumentBuilder();        
-        Document               thinDeltaXML = dBuilder.parse(new ByteArrayInputStream(thin_delta_output));
-        List<Integer>          result = new ArrayList<>();
-        
-        for (Node blockNode : XMLUtils.getXPathNodes(thinDeltaXML, "superblock/diff/*"))
+        try
         {
-            String  nodeName = XMLUtils.getXPathVal(blockNode, "name()");
-            
-            switch(nodeName) 
-            { 
-                case "same": 
-                    addBlockRangeIf(includeSame, blockNode, result);
-                    break; 
-                case "right_only": 
-                    addBlockRangeIf(includeRightOnly, blockNode, result);
-                    break; 
-                case "left_only": 
-                    addBlockRangeIf(includeLeftOnly, blockNode, result);
-                    break; 
-                case "different": 
-                    addBlockRangeIf(includeDifferent, blockNode, result);
-                    break; 
-                default: 
-                    throw new IllegalArgumentException("Invalid block difference result:" + blockNode); 
-            } 
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder        dBuilder = dbFactory.newDocumentBuilder();        
+            Document               thinDeltaXML = dBuilder.parse(new ByteArrayInputStream(thin_delta_output));
+            List<Integer>          result = new ArrayList<>();
+
+            for (Node blockNode : XMLUtils.getXPathNodes(thinDeltaXML, "superblock/diff/*"))
+            {
+                String  nodeName = XMLUtils.getXPathVal(blockNode, "name()");
+
+                switch(nodeName) 
+                { 
+                    case "same": 
+                        addBlockRangeIf(includeSame, blockNode, result);
+                        break; 
+                    case "right_only": 
+                        addBlockRangeIf(includeRightOnly, blockNode, result);
+                        break; 
+                    case "left_only": 
+                        addBlockRangeIf(includeLeftOnly, blockNode, result);
+                        break; 
+                    case "different": 
+                        addBlockRangeIf(includeDifferent, blockNode, result);
+                        break; 
+                    default: 
+                        throw new IllegalArgumentException("Invalid block difference result:" + blockNode); 
+                } 
+            }
+
+            return result;
         }
-        
-        return result;
+        catch (ParserConfigurationException | XPathExpressionException e)
+        {
+            System.err.println("Java configuration issue:" + e);
+            throw new RuntimeException(e);
+        }
+        catch (IOException | SAXException e)
+        {
+            System.err.println("Problem parsing thin_delta output:" + e + "\n" + new String(thin_delta_output));
+            throw new LVM.LVMException(e);
+        }
     }
     
     
@@ -92,59 +105,7 @@ public class LVMBlockDiff
         }        
     }
 
-    /**
-     * getThinPoolInfo ("volg", "volg-thinpool")
-     * @param vgName
-     * @param thinPool
-     * @return
-     */
-    private static LVMThinPool getThinPoolInfo (String vgName, String thinPool) throws InterruptedException, IOException, JSONException
-    {
-        // EXEC lvs --reportformat json -o chunksize,lv_dm_path ${thinPool}
-        byte[]      lvsOutput           = new ExecUtils.ExecuteProcess ("/tmp/", "/sbin/lvs", "--reportformat", "json", "-o", "chunksize,lv_dm_path", vgName + "/" + thinPool).setHideStdOut(true).executeAsBytes();
-        JSONObject  lvsOutputJSON       = new JSONObject(new String (lvsOutput));
-        JSONObject  thinDetails         = lvsOutputJSON.getJSONArray("report").getJSONObject(0).getJSONArray("lv").getJSONObject(0);
-        String      chunkSizeStr        = thinDetails.getString("chunk_size");
-        
-        assert chunkSizeStr.charAt(chunkSizeStr.length() - 1) == 'k' : "Chunk size must be in kB";
-        
-        return new LVMThinPool(thinPool, 
-                               vgName,
-                               Math.round(Float.parseFloat(chunkSizeStr.substring(0, chunkSizeStr.length() - 1))), 
-                               thinDetails.getString("lv_dm_path"));
-    }
     
-    
-    /**
-     * getSnapshotInfo ("volg", "thin_volume_snap2", "thin_volume_snap1")
-     * @param vgName
-     * @param snapshot1
-     * @param snapshot2
-     * @return map of LVMSnapshot keyed by name
-     */
-    private static Map<String, LVMSnapshot> getSnapshotInfo (String vgName, String snapshot1, String snapshot2) throws InterruptedException, IOException, JSONException
-    {
-        // EXEC lvs --reportformat json -o lv_name,vg_name,pool_lv,thin_id volg/thin_volume_snap2 volg/thin_volume_snap1
-        Map<String, LVMSnapshot>    result = new HashMap<>();
-        byte[]      lvsOutput           = new ExecUtils.ExecuteProcess ("/tmp/", "/sbin/lvs", "--reportformat", "json", "-o", "lv_name,vg_name,pool_lv,thin_id", vgName + "/" + snapshot1, vgName + "/" + snapshot2).setHideStdOut(true).executeAsBytes();
-        JSONObject  lvsOutputJSON       = new JSONObject(new String (lvsOutput));
-        JSONArray   lvsnapArray             = lvsOutputJSON.getJSONArray("report").getJSONObject(0).getJSONArray("lv");
-        
-        for (int x = 0 ; x < lvsnapArray.length() ; ++x)
-        {
-            JSONObject  lvsnap = lvsnapArray.getJSONObject(x);
-            String      snapName = lvsnap.getString("lv_name");
-            
-            result.put(snapName, new LVMSnapshot(snapName, 
-                                                     lvsnap.getString("vg_name"), 
-                                                     lvsnap.getString("pool_lv"), 
-                                                     Integer.parseInt(lvsnap.getString("thin_id"))));
-        }
-        
-        return result;
-    }
-
-
     public static class LVMSnapshotDiff 
     {
         private int             chunkSizeKB;
@@ -161,58 +122,12 @@ public class LVMBlockDiff
         public int getChunkSizeKB() 
         {
             return chunkSizeKB;
-        }
+    }
 
         
         public List<Integer> getDifferentBlocks() 
         {
             return differentBlocks;
-        }
-    }
-
-    
-    private static class LVMSnapshot
-    {
-        public String  snapshotName;
-        public String  vgName;
-        public String  poolLV;
-        public int     thinID;
-
-        public LVMSnapshot(String snapshotName, String vgName, String poolLV, int thinID) 
-        {
-            this.snapshotName = snapshotName;
-            this.vgName = vgName;
-            this.poolLV = poolLV;
-            this.thinID = thinID;
-        }
-
-        @Override
-        public String toString() 
-        {
-            return "LVSnapshot " + vgName + "/" + snapshotName + " in " + poolLV + "@" + thinID;
-        }
-    }
-    
-    
-    private static class LVMThinPool
-    {
-        public String  name;
-        public String  vgName;
-        public int     chunkSizeKB;
-        public String  dmPath;
-
-        public LVMThinPool(String name, String vgName, int chunkSizeKB, String dmPath) 
-        {
-            this.name = name;
-            this.vgName = vgName;
-            this.chunkSizeKB = chunkSizeKB;
-            this.dmPath = dmPath;
-        }
-        
-        @Override
-        public String toString() 
-        {
-            return "LVThinPool " + vgName + "/" + name + "  " + chunkSizeKB + " @ " + dmPath;
         }
     }
 }
